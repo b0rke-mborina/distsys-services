dc8bf4cb9708c9ec7be558f5ecc245e00c64d83c
b0rke-mborina <mborina@net.hr>
2022-12-30 10:21:23 +0100

    Created files for services

---
+++ service1.py
@@ -0,0 +1,16 @@
+import aiosqlite
+import aiohttp
+import asyncio
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/")
+async def function(request):
+	return web.json_response({"name": "service1", "status": "OK"}, status = 200)
+
+app = web.Application()
+
+app.router.add_routes(routes)
+
+web.run_app(app, port = 8081)
---
+++ service3.py
@@ -0,0 +1,16 @@
+import aiosqlite
+import aiohttp
+import asyncio
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/")
+async def function(request):
+	return web.json_response({"name": "service3", "status": "OK"}, status = 200)
+
+app = web.Application()
+
+app.router.add_routes(routes)
+
+web.run_app(app, port = 8083)
